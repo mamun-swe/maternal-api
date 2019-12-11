3bada2b7db3f8f4e0ec107f3256cffd4c4c069cb
Mamun-swe <mhmamun166009@gmail.com>
2019-12-12 05:13:09 +0600

    okk

--- api/routes/patient.js
+++ api/routes/patient.js
@@ -4,7 +4,7 @@ const authControllers = require('../controllers/patient/auth')
 const doctorControllers = require('../controllers/patient/doctor')
 
 router.post('/create-account', authControllers.patientRegistration)
-router.get('/logged-patient', authControllers.loggedPatient)
+router.get('/logged-patient/:id', authControllers.loggedPatient)
 
 router.get('/all-doctor', doctorControllers.allDoctor)
 router.get('/single-doctor/:id', doctorControllers.singleDoctor)
